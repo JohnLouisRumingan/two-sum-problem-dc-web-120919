91d6b29b6faa48abc3a0050634d7716416c8b29d
John Louis Rumingan <jmrumingan@gmail.com>
2020-08-04 16:20:52 -0400

    Done.

--- index.js
+++ index.js
@@ -15,3 +15,39 @@ let bruteForceTwoSum = (array, sum) => {
 
     return arrayOfAnswers;
 }
+
+let binarySearchTwoSum = (array, sum) => {
+
+    let sortedArray = array.sort();
+    let arrayOfAnswers = [];
+
+    for(let i=0; i<array.length-1; i++){
+
+
+
+    }
+
+
+    return arrayOfAnswers;
+}
+
+// recursive binaryMatch 
+let binaryMatch = (sortedArray, missingNum) => {
+
+    let middleIndex = sortedArray.length/2;
+
+    if(sortedArray[middleIndex] === missingNum){
+        return true;
+    }
+    else if(sortedArray[middleIndex] > missingNum){
+        //return left half of subarray here 
+        return binaryMatch(sortedArray.slice(middleIndex));
+    }
+    else if(sortedArray[middleIndex] < missingNum){
+        // return right half of original array here
+        return binaryMatch(sortedArray.slice(0, middleIndex));
+    }
+    else if(sortedArray.length <=1 && sortedArray[middleIndex] !== missingNum){
+        return false;
+    }
+}
